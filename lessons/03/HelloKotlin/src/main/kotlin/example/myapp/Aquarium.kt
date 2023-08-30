0b41a6a1a64205543475b2df954a86287a83f956
Aryan Shah <aryanjungshah881@gmail.com>
2023-08-30 14:17:57 -0500

    Aquarium class created

--- lessons/03/HelloKotlin/src/main/kotlin/example/myapp/Aquarium.kt
+++ lessons/03/HelloKotlin/src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,8 +1,14 @@
 package example.myapp
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium(var length:Int = 100,var width: Int = 20,var height: Int = 40) {
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+
+
 }
+
+
 
---
+++ lessons/03/HelloKotlin/src/main/kotlin/example/myapp/Main.kt
@@ -0,0 +1,22 @@
+package example.myapp
+
+fun buildAquarium() {
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+    // default height and length
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+    // default width
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+    // everything custom
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
+
+    val aquarium5 = Aquarium(40,20,10)
+
+}
+
+fun main() {
+    buildAquarium()
+}
