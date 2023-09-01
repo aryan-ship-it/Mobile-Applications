07e38635cd04572cf07aaf677c3334d452410391
Aryan Shah <aryanjungshah881@gmail.com>
2023-09-01 15:17:41 -0500

    make aquarium class open

--- lessons/03/HelloKotlin/src/main/kotlin/example/myapp/Aquarium.kt
+++ lessons/03/HelloKotlin/src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,14 +1,46 @@
 package example.myapp
 
-class Aquarium(var length:Int = 100,var width: Int = 20,var height: Int = 40) {
+open class Aquarium(var length:Int = 100,var width: Int = 20,var height: Int = 40) {
+
+    init{
+        println("aquarium initialized")
+    }
+
+//    init {
+//        println("Volume : ${width * length * height/1000} liters ")
+//    }
+
+    constructor(numberOfFish:Int): this() {
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank/(length* width)).toInt()
+
+    }
+
+    open var volume: Int
+        get() = width * height * length / 1000
+
+        set(value) {
+            height = (value * 1000)/(width *length)
+        }
+
+    open val shape = "rectangle"
+
+    open var water : Double = 0.0
+        get() = volume * 0.9
+
+
+
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+
+        println("Volume: $volume liters")
     }
 
-
 }
+
+
 
 
 
--- lessons/03/HelloKotlin/src/main/kotlin/example/myapp/Main.kt
+++ lessons/03/HelloKotlin/src/main/kotlin/example/myapp/Main.kt
@@ -15,8 +15,18 @@ fun buildAquarium() {
 
     val aquarium5 = Aquarium(40,20,10)
 
+    val aquarium6 = Aquarium(numberOfFish =  29)
+    aquarium6.printSize()
+//    println("Volume : ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
+    aquarium6.volume = 70
+    aquarium6.printSize()
+
+
+
+
 }
 
 fun main() {
     buildAquarium()
 }
+
