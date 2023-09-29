bf43a74266702e50e66bdca9e17bd48cc15ffefd
Aryan Shah <aryanjungshah881@gmail.com>
2023-09-28 22:30:27 -0500

    MulchApp_V1

---
+++ apps/MulchApp/app/src/main/java/com/example/mulchapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mulchapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
