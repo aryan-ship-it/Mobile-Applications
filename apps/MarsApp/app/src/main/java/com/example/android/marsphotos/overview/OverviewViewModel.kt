f87ea0e7c0495e9a8ec15d385ce339b48b3836b1
Aryan Shah <aryanjungshah881@gmail.com>
2023-11-07 15:44:58 -0600

    MarsAppV1

--- apps/MarsApp/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ apps/MarsApp/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -19,6 +19,12 @@ package com.example.android.marsphotos.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsPhoto
+import kotlinx.coroutines.launch
+
+enum class MarsApiStatus{LOADING,ERROR,DONE}
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -26,22 +32,27 @@ import androidx.lifecycle.ViewModel
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the request status
-    val status: LiveData<String> = _status
-    /**
-     * Call getMarsPhotos() on init so we can display status immediately.
-     */
+    val status: LiveData<MarsApiStatus> = _status
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+    val photos: LiveData<List<MarsPhoto>> = _photos
+
+
     init {
         getMarsPhotos()
     }
 
-    /**
-     * Gets Mars photos information from the Mars API Retrofit service and updates the
-     * [MarsPhoto] [List] [LiveData].
-     */
     private fun getMarsPhotos() {
-        _status.value = "Set the Mars API status response here!"
+        viewModelScope.launch {
+            try{
+                _photos.value =  MarsApi.retrofitService.getPhotos()
+                _status.value = MarsApiStatus.DONE
+            }catch(e:Exception){
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
+            }
+        }
     }
 }
